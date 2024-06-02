a38431cf1434b33e3c16a479a9820df11c9d5c44
Matthew Blott <m4ttb@pm.me>
2024-06-02 22:24:51 +0100

    Initial commit

---
+++ demo/src/main/kotlin/com/example/demo/strada/BridgeComponentFactories.kt
@@ -0,0 +1,7 @@
+package com.example.demo.strada
+
+import dev.hotwire.strada.BridgeComponentFactory
+
+val bridgeComponentFactories = listOf(
+  BridgeComponentFactory("overflow-menu", ::OverflowMenuComponent)
+)
